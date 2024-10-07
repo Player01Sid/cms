b6dd4b0b7cef83c92004947116d7ae200e844849
Siddharth Mishra <104432379+Player01Sid@users.noreply.github.com>
2024-10-07 12:21:41 +0530

    Update Home.jsx

--- home/src/pages/Home.jsx
+++ home/src/pages/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <h2>Welcome to my website!</h2>
-      <hr>
+      <hr/>
       <a href="http://cmsid.crabdance.com/wordpress">Wordpress</a>
       <a href="http://cmsid.crabdance.com/joomla">Joomla</a>
     </div>
